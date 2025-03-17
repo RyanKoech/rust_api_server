6d334a0954d3f62874d67efcec06a08df1f4e0b4
RyanKoech <sirryankoech@gmail.com>
2025-03-17 15:09:28 +0300

    setup rocket

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+use rocket_db_pools::{Connection, Database};
+
 mod models;
 mod schema;
 mod repositories;
 
-fn main() {
-    println!("Hello, world!");
+#
+[derive(Database)]
+#[database("postgres")]
+pub struct DbConn(rocket_db_pools::diesel::PgPool);
+
+
+#[rocket::get("/rustaceans")]
+fn get_rustaceans(db: Connection<DbConn>) {
+
+}
+
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .mount("/", rocket::routes![
+            get_rustaceans
+        ])
+        .attach(DbConn::init())
+        .launch()
+        .await;
 }
