d3ae4e8149d1353cbf3e335d0763e1f89a8d5f28
RyanKoech <sirryankoech@gmail.com>
2025-03-25 17:56:34 +0300

    assign roles to users

--- src/models.rs
+++ src/models.rs
@@ -43,8 +43,7 @@ pub struct NewCrate {
     pub description: Option<String>,
 }
 
-#[derive(Queryable, Debug)]
-
+#[derive(Queryable, Debug, Identifiable)]
 pub struct User {
     pub id: i32,
     pub username: String,
@@ -59,7 +58,7 @@ pub struct NewUser {
     pub password: String,
 }
 
-#[derive(Queryable)]
+#[derive(Queryable, Debug, Identifiable)]
 pub struct Role {
     pub id: i32,
     pub code: String,
@@ -74,9 +73,10 @@ pub struct NewRole {
     pub name: String,
 }
 
-#[derive(Queryable)]
+#[derive(Queryable, Associations, Identifiable)]
 #[diesel(belongs_to(User))]
 #[diesel(belongs_to(Role))]
+#[diesel(table_name=users_roles)]
 pub struct UserRole {
     pub id: i32,
     pub user_id: i32,
